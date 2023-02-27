d3eae586aa780db9f0490bda1f51c9404d4d08ae
shadrix <postilga.dima@gmail.com>
2023-02-27 14:05:23 +0200

    feat: added api call to binance

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,4 +1,5 @@
 import { makeAutoObservable} from "mobx";
+import CryptoJS from "crypto-js";
 
 class AuthStore {
     token = "";
@@ -7,8 +8,21 @@ class AuthStore {
         makeAutoObservable(this);
     }
 
-    async login(apiKey: string, apiSecret: string) {
-        this.token = "token";
+    async login(apiKey: string, apiSecret: string) { 
+        const timestamp = Date.now();
+        const params = `timestamp=${timestamp}`;
+        const signature = CryptoJS.HmacSHA256(params, apiSecret).toString(CryptoJS.enc.Hex);
+        const apiUrl = `https://api.binance.com/api/v3/account?${params}&signature=${signature}`;
+        const headers = { 'X-MBX-APIKEY': apiKey };
+      
+        // Make API call using fetch()
+        return fetch(apiUrl, { headers })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data); // Account info object
+            return data;
+          })
+          .catch(error => console.error(error));
     }
 }
 

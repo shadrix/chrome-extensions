5f8884b7f4441cdd8434fe00ba76812d0d5d4286
shadrix <postilga.dima@gmail.com>
2023-03-02 17:53:13 +0200

    feat: improve scan channel logic

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,71 +1,31 @@
-/*
-  This file is the content script of the extension.
-  It is injected into the page when the extension is loaded.
-
-  The content script has direct access to the DOM of the page
-  and is responsible for injecting the React app into the page.
-
-  The content script also has the responsibility of communicating with the React app.
-  This communication is done using the ReactApp class from the messaging module.
-*/
 
 import { ReactApp } from "../messaging/ReactApp";
 
-/*
-  Global variables.
-*/
 let injectedDiv: HTMLDivElement | null = null;
 let injectedIframe: HTMLIFrameElement | null = null;
 let appVisible: boolean = false;
 
-/*
-  Function that injects the React app into the page.
-*/
 function injectReactApp(): void {
-  /*
-    Return if the app is already injected.
-  */
   if (injectedDiv) {
     return;
   }
-  /*
-    Creating the div element that will contain the React app iframe.
-  */
   const div = document.createElement("div");
   div.setAttribute("id", "react-app-div");
   div.setAttribute("class", "opened");
-  /*
-    Creating the iframe element that will contain the React app.
-  */
   const iframe = document.createElement("iframe");
   iframe.setAttribute("id", "react-app-iframe");
   iframe.setAttribute("src", chrome.runtime.getURL("index.html"));
-  /*
-    Injecting the custom CSS to the page.
-  */
   const link = document.createElement("link");
   link.setAttribute("rel", "stylesheet");
   link.setAttribute("href", chrome.runtime.getURL("index.css"));
   document.querySelector("head")?.appendChild(link);
-  /*
-    Appending the iframe to the div.
-    Appending the div to the body.
-  */
   div.appendChild(iframe);
   document.querySelector("body")?.appendChild(div);
-  /*
-    Updating the global variables.
-  */
   injectedDiv = div;
   injectedIframe = iframe;
   appVisible = true;
 }
 
-/*
-  Adding a keydown event listener to the window.
-  This event listener will be used to show/hide the app.
-  The event listener will only be added if the window is the top window.
-*/
 if (window.self === window.top) {
   const KEY_BIND = "Q";
   window.addEventListener("keyup", (event) => {
@@ -86,47 +46,35 @@ if (window.self === window.top) {
   });
 }
 
-/************************************
-  Make changes as you wish from here
-  to communicate with the React app.
-*/
-
 function startConnection(): void {
-  /*
-    Instantiate a new ReactApp object.
-    This object will be used to communicate with the React app.
-  */
   const reactApp = new ReactApp(injectedIframe);
   reactApp.onMessage((message) => {
-    console.log("Message received from child iframe: ", message);
-    /*
-      Handle the message based on its type.
-      Each message type should be handled in a different case.
-
-      Create new message types in the messaging/messageTypes.ts file.
-    */
     switch (message.type) {
       case "SCAN_DOM":
         const messageElements = Array.from(
           document.querySelectorAll("div[id^='message']")
         );
-
+      
         const filteredMessages = messageElements.filter((message) => {
           const metaElement = message.querySelector(".text-content.with-meta");
           const hashtagElement = message.querySelector(".text-entity-link");
-
+        
           if (
             metaElement &&
             metaElement.textContent?.includes("🚨") &&
             hashtagElement &&
             hashtagElement.textContent?.startsWith("#")
           ) {
-            return true;
+            const limitOrderElement = metaElement.querySelector(":nth-child(3)");
+            const limitOrderText = limitOrderElement !== null ? limitOrderElement.nextSibling?.textContent?.trim() || "" : "";
+            const isLimitOrderExecuted = limitOrderText.includes("executed");
+        
+            return !isLimitOrderExecuted;
           }
-
+        
           return false;
         });
-
+       
         filteredMessages.forEach((messageElement) => {
           if (!messageElement.querySelector(".crypto")) {
             const button = document.createElement("button");
